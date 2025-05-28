0cf912f2751fcc7f39f17144246e4d6b41f4b70b
Jesus <jesus.cont5648@gmail.com>
2025-05-28 13:38:56 -0500

    feat: added Query Service from Appointment

---
+++ src/main/java/com/platform/dentify/patientattention/application/internal/AppointmentQueryServiceImpl.java
@@ -0,0 +1,38 @@
+package com.platform.dentify.patientattention.application.internal;
+
+import com.platform.dentify.iam.infrastructure.security.AuthenticatedUserProvider;
+import com.platform.dentify.patientattention.domain.model.aggregates.Appointment;
+import com.platform.dentify.patientattention.domain.model.queries.GetAllAppointmentsByPatientAndUserIdQuery;
+import com.platform.dentify.patientattention.domain.services.AppointmentQueryService;
+import com.platform.dentify.patientattention.infrastructure.repositories.AppointmentRepository;
+import com.platform.dentify.patientattention.infrastructure.repositories.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AppointmentQueryServiceImpl implements AppointmentQueryService {
+
+    private final AppointmentRepository appointmentRepository;
+    private final PatientRepository patientRepository;
+    private final AuthenticatedUserProvider authenticatedUserProvider;
+
+    public AppointmentQueryServiceImpl(AppointmentRepository appointmentRepository, PatientRepository patientRepository, AuthenticatedUserProvider authenticatedUserProvider) {
+        this.appointmentRepository = appointmentRepository;
+        this.patientRepository = patientRepository;
+        this.authenticatedUserProvider = authenticatedUserProvider;
+    }
+
+    @Override
+    public List<Appointment> handle(GetAllAppointmentsByPatientAndUserIdQuery query) {
+        Long userId = authenticatedUserProvider.getCurrentUserId();
+        if (patientRepository.findById(query.patientId()).isEmpty()) {
+            throw new IllegalArgumentException("Patient not found");
+        }
+        if (patientRepository.findByIdAndUser_Id(query.patientId(), userId).isEmpty()) {
+            throw new IllegalArgumentException("Patient not found");
+        }
+
+        return appointmentRepository.findAllByPatientId(query.patientId());
+    }
+}
