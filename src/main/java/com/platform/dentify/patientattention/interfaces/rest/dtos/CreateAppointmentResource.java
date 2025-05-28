5e105ddbcf532671dab083bc0fe7a92a8de63f03
Jesus <jesus.cont5648@gmail.com>
2025-05-28 18:14:34 -0500

    feat: create resources from Create command services

---
+++ src/main/java/com/platform/dentify/patientattention/interfaces/rest/dtos/CreateAppointmentResource.java
@@ -0,0 +1,11 @@
+package com.platform.dentify.patientattention.interfaces.rest.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+
+public record CreateAppointmentResource(LocalDateTime appointmentDate, String reason,
+                                        @JsonFormat(pattern = "HH:mm") LocalTime duration, Long patientId) {
+}
