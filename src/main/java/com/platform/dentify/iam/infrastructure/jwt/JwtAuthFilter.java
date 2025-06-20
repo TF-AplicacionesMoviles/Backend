97a840b84686be96dc2507d1b202070e53ebda74
Jesus <jesus.cont5648@gmail.com>
2025-06-20 01:53:23 -0500

    feat: updated expiration of jwt

--- src/main/java/com/platform/dentify/iam/infrastructure/jwt/JwtAuthFilter.java
+++ src/main/java/com/platform/dentify/iam/infrastructure/jwt/JwtAuthFilter.java
@@ -23,14 +23,13 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("JWT Filter ejecutado"); // <--- AÑADE ESTO
 
 
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String token = authHeader.substring(7);
-            System.out.println("Authorization Header: " + authHeader);
+
 
             if (jwtService.isTokenValid(token)) {
                 Long userId = Long.valueOf(jwtService.extractUserId(token));
